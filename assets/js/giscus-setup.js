21b4564dd54cbf3dfeb3300121c4929338e7c194
janpetrscience <69957278+janpetrscience@users.noreply.github.com>
2025-12-12 16:59:19 +0000

    Deploying to gh-pages from @ janpetrscience/janpetrscience.github.io@a82d68d2321ef6731a2b884cfeec1e97ca7a20fe 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "janpetrscience/webpage",
+    "data-repo": "janpetrscience/janpetrscience.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
